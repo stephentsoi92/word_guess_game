ff5b1ba19ed44928ffccb36e5df1cb7b1fe22712
stephentsoy92 <alanstephentsoy@gmail.com>
2019-07-02 23:48:22 -0400

    Update the .js document for better functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,80 @@
-/*Quiero hacer un juego en el cual las personas adivinen el nombre de una persona al presionar las letras de el teclado y que reste vidas por cada vez que la letra no esta mencionada en dicho nombre y en caso de que la letra este mencionada una o multiples veces hacerla aparecer 
+/*
+Quiero hacer un juego en el cual el usuario adivine el nombre de algo al presionar las letras de el teclado y que reste vidas por cada vez que la letra no esta mencionada en dicho nombre y en caso de que la letra este mencionada una o multiples veces hacerla aparecer 
 */
  
-//1. have the name of the person one needs to guess (store)
-  var names = ["ferrari", "mercedes", "redbull", "mclaren", "Renault"]
+//1. have the name of the things the user needs to guess
+function randomName()
+{  
+var names = ["ferrari", "mercedes", "redbull", "mclaren", "Renault"];
 
-//2. Choose a ramdom name
-  var randomName = names[Math.floor(Math.random()*names.length)];
+  //2. Choose a ramdom name make the keyboard strokes active and store the string
+  var name = names.length;
+  var randomName = Math.floor(Math.random() * name)
+    return names[ randomName ];
+}
+var random = randomName()
+var randomLength = random.length
+console.log(random)
+// show dashes in id wordToGuess
+var uniqueKey = new Set([])
+for (var i=0; i < randomLength; i++)
+{
+var dashes = document.createElement("span");
+dashes.id = "span"+i;
+dashes.className = random[i];
+dashes.innerHTML = "_ ";
+var dashSpace = document.getElementById("wordToGuess");
+dashSpace.appendChild(dashes)
+uniqueKey.add(randomLength[i])
+}
+
+
+var playerwins = "";
+playerwinsCounter = 0;
+var numGuessesRem = "";
+numGuessesRem = 12;
+
+var match = document.getElementById("numWins")
+match.innerHTML = playerwinsCounter;
+
+var noMatch = document.getElementById("livesRem")
+noMatch.innerHTML = numGuessesRem;
+
+var lettersGuessed = [];
+var correctLetters = new Set([]);
+document.onkeyup = function (event)
+{
   
-  //console.log(randomName);
-
-
-//3. make the keyboard strokes active and store the string
-  document.onkeyup = function(event)
-  {
-    var letterGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    document.getElementById("guessLetter").innerHTML = letterGuess;
-
 //5. record the keyboard strokes (once pressed it cannot be pressed again)
-    var answeredLetters = "";
-  // THIS IS NOT WORKING
-    answeredLetters += letterGuess;
-    console.log(answeredLetters)
-  }
+  var keyStroke = event.key.toLowerCase();
+  lettersGuessed += keyStroke;
+  document.getElementById("guessLetter").innerHTML = keyStroke;
+
 //4. make letters appear as they pressed the keyboard and store them
-  for (var i=0; i < randomName.length; i++)
+  document.getElementById("guessLetter").innerHTML = lettersGuessed;
+  var selected = document.getElementById(keyStroke); 
+
+  if (selected.length > 0)
   {
-    var dashes = document.createElement("span");
-    dashes.id = "span"+i;
-    dashes.className = randomName[i];
-    dashes.innerHTML = "_ ";
-    var dashArea = document.getElementById("wordToGuess");
-    //error dashArea null
-    dashArea.appendChild(dashes);
+    for (var j=0; j < selected.length; j++)
+    {
+      selected.length[j].innerHTML = keyStroke
+    }
+    correctLetters.add(keyStroke);
+    if(correctLetters.size === uniqueKey.size)
+    {
+      alert("Good Job! Press ok to continue playing");
+      location.reload(true);
+      playerwinsCounter++;
+    }
+    else
+    {
+
+    }
   }
-  
+}
+
 //5. check letters
-  var guess = ""
-
-  document.getElementById("guessLetter").innerHTML = guess;
-  
-  
-
 //6. 
 //7. if it matches with the name make letters appear on the dashes
-//8. make a counter for the lifes and take lifes away if the letters pressed are do not match with the name
-  
-
-//Array for the names that are going to be guessed
-//var names = ["Charles Leclerc", "Max Verstapen", "Pierre Gasly", "Sebatian Vettel"]
-  
-  
-//section that will record the player strokes on the keyboard 
-
-
-/* this syntax makes the computer choose a ramdom name from the array name
-var randomName = names[Math.floor(Math.random()*names.length)];
-console.log(randomName);
-*/
-//iteration to look through the correct letters of the name with a for loop test if a letter of the name is pressed return true
-//  for (var i=0; i < names[0].length; i++)
-
-
-//make an alert to see if the letter pressed is good
-// if (strokeGuess === "c"){
-//      var charles = function ()
-  
+//8. make a counter for the lifes and take lifes away if the letters pressed 
